09adf3cf1d5119dc650b93c49abad97c4085cd60
Kanik Pathak <120249077+guyfrompluto@users.noreply.github.com>
2023-11-11 19:45:51 +0530

    added animation in home page

---
+++ 1. Landing Page/script.js
@@ -0,0 +1,17 @@
+
+        const i3 = document.getElementById("i3");
+        const text = "Inquire Interact Inspire";
+        let temp = 0;
+
+        function animateText() {
+            if (temp > text.length) {
+            i3.innerHTML="";
+             temp=0;
+            }
+            i3.innerHTML += text.charAt(temp);
+            temp++;
+            setTimeout(animateText, 200);
+        }
+
+        animateText();
+    
